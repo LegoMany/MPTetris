4bbb5928ddb1f5f1348fa5c5a0c99aa0e7e33752
Sorin Loghin <sorin.loghin@in2code.de>
2020-10-12 20:08:54 +0200

    Reformat shape files

--- client/src/shapes/LRShape.ts
+++ client/src/shapes/LRShape.ts
@@ -1,5 +1,5 @@
-import {AbstractShape} from 'shapes/AbstractShape'
-import {Cell} from 'shapes/Cell'
+import { AbstractShape } from 'shapes/AbstractShape'
+import { Cell } from 'shapes/Cell'
 
 export class LRShape extends AbstractShape {
   cells = [
--- client/src/shapes/LShape.ts
+++ client/src/shapes/LShape.ts
@@ -1,9 +1,9 @@
-import {AbstractShape} from 'shapes/AbstractShape'
-import {Cell} from 'shapes/Cell'
+import { AbstractShape } from 'shapes/AbstractShape'
+import { Cell } from 'shapes/Cell'
 
 export class LShape extends AbstractShape {
-    cells = [
-        [new Cell(1, 1)],
-        [new Cell(2, 1), new Cell(2, 2), new Cell(2, 3)],
-    ]
+  cells = [
+    [new Cell(1, 1)],
+    [new Cell(2, 1), new Cell(2, 2), new Cell(2, 3)],
+  ]
 }
