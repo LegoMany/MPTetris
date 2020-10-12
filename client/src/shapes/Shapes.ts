f316ad01b1b4d0d2f7e893f373d94ba9714d5fc3
Sorin Loghin <sorin.loghin@in2code.de>
2020-10-12 20:09:03 +0200

    Add SquareShape to Shapes list

--- client/src/shapes/Shapes.ts
+++ client/src/shapes/Shapes.ts
@@ -1,9 +1,10 @@
-import {LRShape} from "./LRShape"
-import {LShape} from "./LShape"
-import {TShape} from "./TShape"
-import {ZShape} from "./ZShape"
-import {SShape} from "./SShape"
-import {IShape} from "./IShape"
+import { LRShape } from "./LRShape"
+import { LShape } from "./LShape"
+import { TShape } from "./TShape"
+import { ZShape } from "./ZShape"
+import { SShape } from "./SShape"
+import { IShape } from "./IShape"
+import { SquareShape } from "./SquareShape"
 
 export let Shapes = {
   LShape,
@@ -12,5 +13,6 @@ export let Shapes = {
   ZShape,
   SShape,
   IShape,
+  SquareShape,
 }
 
